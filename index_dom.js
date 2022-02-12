414da0d03486c59fc4c99d0aec746f3d85cf0338
ezritz <lidia.edith.escoto@gmail.com>
2022-02-11 19:27:36 -0600

    Fix hide menu and add transition with opacity

--- index_dom.js
+++ index_dom.js
@@ -1,7 +1,7 @@
 import hamburgerMenu from "./menu_hamburguesa.js";
 
 document.addEventListener("DOMContentLoaded", e => {
-    hamburgerMenu(".panel-btn", ".panel");
+    hamburgerMenu(".panel-btn", ".panel", ".menu a");
 })
 
 
